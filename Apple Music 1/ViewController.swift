399babfd2f5e9aaa687ea38e4fec76f41688d8b8
Berat <inceberat626@gmail.com>
2023-03-09 04:35:14 +0300

    Initial Commit

---
+++ Apple Music 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Music 1
+//
+//  Created by Berat Tugay İnce on 9.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Apple Music 1UITests/Apple_Music_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Apple_Music_1UITestsLaunchTests.swift
+//  Apple Music 1UITests
+//
+//  Created by Berat Tugay İnce on 9.03.2023.
+//
+
+import XCTest
+
+final class Apple_Music_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
